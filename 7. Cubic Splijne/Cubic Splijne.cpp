c1db0a3f452521e17ef1644465f79f60ae0dd92f
Kirill <83186959+ShinyEagle77@users.noreply.github.com>
2022-10-09 13:54:07 +0300

    Add files via upload

---
+++ 7. Cubic Splijne/Cubic Splijne.cpp
@@ -0,0 +1,301 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <string>
+#include <exception>
+#include <map>
+#include <random>
+#include <vector>
+
+using namespace std;
+
+uniform_int_distribution<> uid(-10, 10);
+
+static int nValue = 4;
+static int nStep = 1;
+static int iterationCount = 0;
+
+double S(const double& _h, const double& _m, const double& _mPrev, const double& _x, 
+    const double& _xPrev, const double& _y, const double& _yPrev)
+{
+    return (_mPrev * pow(_x, 3) - _m * pow(_xPrev, 3) + 6 * _yPrev * _x - 6 * _y * _xPrev + _m * pow(_h, 2) * _xPrev - _mPrev * pow(_h, 2) * _x) / (6 * _h);
+}
+
+double S1(const double& _h, const double& _m, const double& _mPrev, const double& _x,
+    const double& _xPrev, const double& _y, const double& _yPrev)
+{
+    return (_m * pow(_xPrev, 2) - _mPrev * pow(_x, 2) + 2 * _y - 2 * _yPrev + _mPrev * pow(_h, 2) / 3 - _m * pow(_h, 2) / 3) / (2 * _h);
+}
+
+double S2(const double& _h, const double& _m, const double& _mPrev, const double& _x, const double& _xPrev)
+{
+    return (_mPrev * _x - _m * _xPrev) / (2 * _h);
+}
+
+double S3(const double& _h, const double& _m, const double& _mPrev)
+{
+    return (_m - _mPrev) / (6 * _h);
+}
+
+double P(const double& h_i, const double& h_i_p, const double& P_i)
+{
+    return -h_i_p / (2 * (h_i + h_i_p) + P_i * h_i);
+}
+
+double Q(const double& h_i, const double& h_i_p, const double& P_i, const double& Q_i, const double& Y_i) 
+{
+    return (6 * Y_i - h_i * Q_i) / (2 * (h_i + h_i_p) + P_i * h_i);
+}
+
+
+//void PrintMatrix(double** _matrix, const int& node)
+//{
+//	for (int i = 0; i < node; ++i)
+//	{
+//		for (int j = 0; j < node; ++j)
+//		{
+//			cout << setw(static_cast<streamsize>(4)) << setprecision(4) << fixed << _matrix[i][j] << ' ';
+//		}
+//		cout << setw(static_cast<streamsize>(4)) << setprecision(4) << fixed << _matrix[i][node] << ' ' << endl;
+//        cout << endl;
+//	}
+//	cout << endl;
+//}
+
+void PrintMatrix(double** _final, const int& node)
+{
+    for (int i = 0; i < node - 1; i++) {
+        cout << setprecision(6) << fixed << "S" << i + 1 << "(x)=( " << _final[i][2] << " )*x^3+( " << _final[i][3]
+            << " )*x^2+( " << _final[i][4] << " )*x+( " << _final[i][5] << " ); {"
+            << setprecision(2) << fixed << _final[i][0] << "<=x<=" << _final[i][1] << "}\n";
+    }
+}
+
+void PrintPoints(double** _matrix, const int& node)
+{
+    cout << "i: ";
+    for (int i = 1; i <= node; ++i)
+    {
+        cout << setw(7) << i << ' ';
+    }
+    cout << endl << "X: ";
+    for (int i = 0; i < node; ++i)
+    {
+        cout << setw(7) << _matrix[0][i] << ' ';
+    }
+    cout << endl << "Y: ";
+    for (int i = 0; i < node; ++i)
+    {
+        cout << setw(7) << setprecision(4) << fixed << _matrix[1][i] << ' ';
+    }
+    cout << endl << endl;
+}
+
+//vector<double> CountSplineLength (double** _result, const int& node)
+//{
+//    vector<double> _h;
+//    for (int i = 0; i < node - 1; ++i)
+//    {
+//        _h.push_back(_result[0][i + 1] - _result[0][i]);
+//    }
+//
+//    return _h;
+//}
+//
+//vector<double> CountY (double **_result, const vector<double>& _h, const int& node)
+//{
+//    vector<double> _y;
+//    for (size_t i = 0; i < node - 2; ++i)
+//    {
+//        double pushValue = (_result[1][i + 2] - _result[1][i + 1]) / _h[i + 1] - (_result[1][i + 1] - _result[1][i]) / _h[i];
+//        _y.push_back(pushValue);
+//    }
+//
+//    return _y;
+//}
+//
+//vector<double> CountP (const vector<double>& _h, const int& node)
+//{
+//    vector<double> _p;
+//    _p.push_back(0);
+//    for (size_t i = 1; i < node - 2; ++i)
+//    {
+//        double pushValue = -_h[i] / (2 * (_h[i - 1] + _h[i]) + _p[i - 1] * _h[i - 1]);
+//        _p.push_back(pushValue);
+//    }
+//
+//    return _p;
+//}
+//
+//vector<double> CountQ (const vector<double>& _y, const vector<double>& _p, const vector<double>& _h, const int& node)
+//{
+//    vector<double> _q;
+//    _q.push_back(0);
+//    for (size_t i = 1; i < _h.size(); ++i)
+//    {
+//        double pushValue = (6 * _y[i - 1] - _h[i - 1] * _q[i - 1]) / (2 * (_h[i - 1] + _h[i]) + _p[i - 1] * _h[i - 1]);
+//        _q.push_back(pushValue);
+//    }
+//
+//    return _q;
+//}
+//
+//vector<double> CountM(const vector<double>& _p, const vector<double>& _q, const int& node)
+//{
+//    vector<double> _m(node);
+//    _m[node - 1] = 0;
+//    for (int i = node - 1; i > 0; --i)
+//    {
+//        _m[i - 1] = _p[i - 1] * _m[i] + _q[i - 1];
+//    }
+//
+//    /*reverse(_m.begin(), _m.end());*/
+//
+//    return _m;
+//}
+
+
+double** CubicSpline(double** _result, const int& node) // MAKE DIFFERENT FUNCTION FOR A MANUAL INPUT AND AUTOMATIC
+{
+
+	//PrintMatrix(_result, node);
+    PrintPoints(_result, node);
+
+    auto* P_i = new double[node - 1];
+    auto* Q_i = new double[node - 1];
+    auto* h_i = new double[node - 1];
+
+    for (int i = 0; i < node - 1; i++) 
+    {
+        h_i[i] = _result[0][i + 1] - _result[0][i];
+    }
+
+    P_i[0] = 0, Q_i[0] = 0;
+
+    for (int i = 0; i < node - 2; i++) 
+    {
+        double Y_i = (_result[1][i + 2] - _result[1][i + 1]) / (h_i[i + 1]) - (_result[1][i + 1] - _result[1][i]) / (h_i[i]);
+        P_i[i + 1] = P(h_i[i], h_i[i + 1], P_i[i]);
+        Q_i[i + 1] = Q(h_i[i], h_i[i + 1], P_i[i], Q_i[i], Y_i);
+    }
+
+    auto* m_i = new double[node];
+    m_i[node - 1] = 0;
+    for (int i = node - 1; i > 0; i--) 
+    {
+        m_i[i - 1] = P_i[i - 1] * m_i[i] + Q_i[i - 1];
+    }
+
+    auto** _final = new double* [node - 1];
+    for (int i = 0; i < node - 1; ++i)
+    {
+        _final[i] = new double[6]; 
+    }
+
+    for (int i = 0; i < node - 1; i++) {
+        _final[i][0] = _result[0][i];
+        _final[i][1] = _result[0][i + 1];
+        _final[i][2] = S3(h_i[i], m_i[i + 1], m_i[i]);
+        _final[i][3] = S2(h_i[i], m_i[i + 1], m_i[i], _result[0][i + 1], _result[0][i]);
+        _final[i][4] = S1(h_i[i], m_i[i + 1], m_i[i], _result[0][i + 1], _result[0][i], _result[1][i + 1], _result[1][i]);
+        _final[i][5] = S(h_i[i], m_i[i + 1], m_i[i], _result[0][i + 1], _result[0][i], _result[1][i + 1], _result[1][i]);
+    }
+
+	return _final;
+}
+
+double** MatrixHub(const int& node, const int& nGroup, const int& a, const int& b)
+{
+    auto** _result = new double* [2];
+    for (int i = 0; i < 2; ++i)
+    {
+        _result[i] = new double[node];
+    }
+
+    for (int x = 0; x < node; x += nStep)
+    {
+        _result[0][x] = x + a;
+        _result[1][x] = nGroup * _result[0][x] + sin(_result[0][x]);                   // Main function to fill;
+       /* _result[1][x - a] = x * sin(x);*/
+    }
+    return CubicSpline(_result, node);
+}
+
+double** MatrixHub(const int& node, const int& a, const int& b)
+{
+    auto** _result = new double* [2];
+    for (int i = 0; i < 2; ++i)
+    {
+        _result[i] = new double[node];
+    }
+
+    for (int x = a; x <= b; x += nStep)
+    {
+        _result[0][x - a] = x;
+        _result[1][x - a] = cos(x) + sin(x);
+    }
+    return CubicSpline(_result, node);
+}
+
+pair<int, int> CountTableSize(const int& nStudent)
+{
+    const int a = nStudent,
+			b = nStudent + nValue;                                // nValue = 4;
+	/*const int a = -4, b = 4;*/
+
+    return make_pair(a, b);
+}
+
+pair<int, int> CountTableSize()
+{
+    random_device dev;
+    mt19937 _randengine(dev());
+
+    int a = uid(_randengine),
+	b = uid(_randengine) + nValue;
+
+    if (a > b)
+    {
+        int temp = b;
+        a = b;
+        b = temp;
+    }
+    return make_pair(a, b);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+    cout << "Аппроксимация табличной функции кубическими сплайнами. " << endl
+        << "Для ввода параметров студента, введите 0. Для ручного ввода параметров и рандомных точек: 1" << endl;
+
+    int inputType;
+    cin >> inputType;
+
+    if (inputType == 0)
+    {
+        cout << "Введите номер группы и номер студента (через пробел)" << endl;
+    	int nGroup, nStudent;
+	    cin >> nGroup >> nStudent;                                      // Defined as StudentData
+
+	    const int node = abs(CountTableSize(nStudent).first - CountTableSize(nStudent).second) + 1;
+
+    	auto** _final = MatrixHub(node, nGroup, CountTableSize(nStudent).first, CountTableSize(nStudent).second);
+    	PrintMatrix(_final, node);
+    }
+    else if (inputType == 1)
+    {
+        cout << "Введите значение N для задания интервала, шаг итерации: " << endl;
+        cin >> nValue >> nStep;
+
+        const int node = abs(CountTableSize().first - CountTableSize().second) + 1;
+
+        auto** _final = MatrixHub(node, CountTableSize().first, CountTableSize().second);
+        PrintMatrix(_final, node);
+    }
+   
+
+    return 0;
+
+}
---
+++ 9 Devariative/9 Devariative.cpp
@@ -0,0 +1,284 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <random>
+#include <string>
+
+using namespace std;
+
+uniform_real_distribution<> uid(-10, 10);
+
+#define StudentData nGroup, nStudent
+#define MatrixData _matrix, node
+
+void PrintMatrix(double** _matrix, const int& node)
+{
+    for (int i = 0; i < node; ++i)
+    {
+        for (int j = 0; j < node; ++j)
+        {
+            cout << setw(12) << setprecision(4) << fixed << _matrix[i][j] << " * x ^ " + to_string(node - j) << ' ';
+        }
+        cout << setw(7) << setprecision(4) << fixed << _matrix[i][node] << ' ' << endl;
+    }
+    cout << "\n";
+}
+
+void PrintPoints(double** _matrix, const int& node)
+{
+    cout << "X: ";
+    for (int i = 0; i < node; ++i)
+    {
+        cout << setw(7) << setprecision(2) << fixed << _matrix[i][0] << ' ';
+    }
+    cout << endl << "Y: ";
+    for (int i = 0; i < node; ++i)
+    {
+        cout << setw(7) << setprecision(2) << fixed << _matrix[i][node] << ' ';
+    }
+    cout << endl << endl;
+}
+
+double** CreateMatrix(const int& node, const int& nGroup, const int& nStudent, const int& inputType)
+{
+    setlocale(LC_ALL, "rus");
+
+    random_device dev;
+    mt19937 _randengine(dev());
+
+    auto** _matrix = new double* [node];
+
+    for (int i = 0; i < node; ++i)
+    {
+        _matrix[i] = new double[node];
+    }
+
+    if (inputType == 0) // studentParameters
+    {
+        if (node > 5 || node < 1)
+        {
+            cout << "Неверное количество точек" << endl;
+            exit(10);
+        }
+        else
+        {
+            if (node >= 2 && node <= 5) {
+                for (int i = 0; i < node; i++) {
+                    _matrix[0][i] = -2;
+                }
+                _matrix[0][node] = nStudent;
+            }
+            if (node >= 2 && node <= 5) {
+                for (int i = 0; i < node; i++) {
+                    _matrix[1][i] = -1;
+                }
+                _matrix[1][node] = nGroup;
+            }
+            if (node >= 3 && node <= 5) {
+                for (int i = 0; i < node; i++) {
+                    _matrix[2][i] = 0;
+                }
+                _matrix[2][node] = -1;
+            }
+            if (node >= 4 && node <= 5) {
+                for (int i = 0; i < node; i++) {
+                    _matrix[3][i] = 1;
+                }
+                _matrix[3][node] = nStudent;
+            }
+            if (node == 5) {
+                for (int i = 0; i < node; i++) {
+                    _matrix[4][i] = 2;
+                }
+                _matrix[4][node] = nGroup;
+            }
+        }
+    }
+    else if (inputType == 1) // Random
+    {
+        for (int i = 0; i < node; ++i)
+        {
+            for (int j = 0; j < node; ++j)
+            {
+                _matrix[i][j] = uid(_randengine);
+            }
+            _matrix[i][node] = uid(_randengine);
+        }
+        
+    }
+    PrintPoints(MatrixData);
+
+    for (int i = 0; i < node; i++) {
+        for (int j = 0; j < node; j++) {
+            _matrix[i][j] = pow(_matrix[i][j], node - 1 - j);
+        }
+    }
+    PrintMatrix(MatrixData);
+
+    return _matrix;
+}
+
+double* FindRoots(double** matrix, int n)
+{
+    int iter, i, j;
+    double* roots = new double[n], summ = 0;
+    for (iter = 0; iter < n; iter++) {
+        for (i = iter; i < n; i++) {
+            double temp = matrix[i][iter];
+            if (temp != 0) {
+                for (j = iter; j < n; j++) {
+                    matrix[i][j] = matrix[i][j] / temp;
+                }//Нормирование элементов в строке.
+                matrix[i][n] = matrix[i][n] / temp;
+            }
+        }
+        for (i = iter + 1; i < n; i++) {
+            if (matrix[i][iter] != 0) {
+                for (j = iter; j < n; j++) {
+                    matrix[i][j] = matrix[i][j] - matrix[iter][j];//Функция вычитания для получения нуля.
+                    summ = summ + abs(matrix[i][j]);//Поиск строки с нулевыми элементами.
+                }
+                matrix[i][n] = matrix[i][n] - matrix[iter][n];
+                if (summ == 0) {
+                    cout << "Error, null line!";
+                    exit(1);
+                }
+            }
+        }
+    }
+    summ = 0;
+    for (iter = n - 1; iter >= 0; iter--) {
+        roots[iter] = matrix[iter][n] - summ; //Подстановка корней обратной последовательностью.
+        summ = 0;
+        if (iter > 0) {
+            for (i = n - 1; i >= iter; i--) {
+                summ = summ + matrix[iter - 1][i] * roots[i];
+            }
+        }
+    }
+    return roots;
+}
+
+void PrintRootsFinal(const double* roots, const int& der_lvl, const int& node)
+{
+    int i;
+    cout << "P";
+    if (der_lvl < 4) 
+    {
+        for (i = 0; i < der_lvl; i++) 
+        {
+            cout << "'";
+        }
+    }
+    else 
+    {
+        cout << "^(" << der_lvl << ")";
+    }
+    cout << "(x) = ";
+    if (der_lvl < node) 
+    {
+        for (i = 0; i < node - der_lvl; i++) 
+        {
+            if (i != 0) 
+            {
+                cout << "+";
+            }
+            cout << "(" << setw(7) << setprecision(4) << fixed << roots[i] << " )*x^(" << node - 1 - i - der_lvl << ")";
+        }
+    }
+    else 
+    {
+        cout << "0";
+    }
+    cout << "\n";
+}
+
+void Root_output(const double* roots, const int& der_lvl, const int& n)
+{
+    int i;
+    cout << "P";
+    if (der_lvl < 4) {
+        for (i = 0; i < der_lvl; i++) {
+            cout << "'";
+        }
+    }
+    else {
+        cout << "^(" << der_lvl << ")";
+    }
+    cout << "(x) = ";
+    if (der_lvl < n) {
+        for (i = 0; i < n - der_lvl; i++) {
+            if (i != 0) {
+                cout << ' ';
+            }
+            cout << ' ' << setw(8) << setprecision(3) << fixed << roots[i] << "*x^" << n - 1 - i - der_lvl << "";
+        }
+    }
+    else {
+        cout << "0";
+    }
+    cout << "\n";
+}
+
+void Derivative(const double* roots, const int& n) // TO DO
+{
+    int der_lvl = n;
+
+    auto** der_cf = new double* [der_lvl + 1];
+    for (int i = 0; i < der_lvl + 1; i++) 
+    {
+        der_cf[i] = new double[n];
+    }
+
+    for (int i = 0; i < n; i++) 
+    {
+        der_cf[0][i] = roots[i];
+    }
+
+    for (int lvl = 1; lvl <= der_lvl; lvl++) 
+    {
+        for (int i = 0; i < n; i++) 
+        {
+            if (i < n - lvl) 
+            {
+                der_cf[lvl][i] = der_cf[lvl - 1][i] * (n - 1 - i);
+            }
+            else 
+            {
+                der_cf[lvl][i] = 0;
+            }
+        }
+    }
+    for (int i = 0; i < der_lvl; i++) 
+    {
+        Root_output(der_cf[i], i, n);
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    cout << "Решение производной." << endl;
+    cout << "Введите размер таблицы, номер группы и номер студента, тип ввода: 0 - параметры студента, 1 - рандом (через пробел)" << endl;
+
+    int node, nGroup, nStudent, inputType; // nStudent & nGroup defined as StudentData;
+    cin >> node >> nGroup >> nStudent >> inputType;
+
+    auto** _matrix = CreateMatrix(node, StudentData, inputType);
+    auto* _roots = FindRoots(MatrixData);
+
+    cout << "Корни системы: ";
+    for (int i = 0; i < node; ++i)
+    {
+        cout << setw(5) << _roots[i] << ' ';
+    }
+
+    cout << endl << endl;
+
+    Derivative(_roots, node);
+
+    return 0;
+}
+
+
