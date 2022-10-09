94254d82fa7b9be9eaaf6002d31ad7f693005b31
Kirill <83186959+ShinyEagle77@users.noreply.github.com>
2022-10-09 13:53:50 +0300

    Add files via upload

---
+++ 5.2 IterationMethodForNonLinear/IterationMethodForNonLinear.cpp
@@ -0,0 +1,166 @@
+﻿#include <iostream>
+#include <cmath>
+#include <exception>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+static const double epsAcc = 0.00000001;
+static int simpleIterCount;
+
+#define StudentParameters nStudent, nGroup
+#define Borders _firstBorder, _secondBorder
+#define SignMax  equationSign, maxBasicValue
+
+//double PutInMain(const double& value, 
+//	const int& nStudent, const int& nGroup)
+//{
+//	return (pow(value, 3) / (2 * nStudent * nGroup))
+//		+ ((nStudent + nGroup) / 2);
+//}
+//
+//double InFirstDevariative (const double& value,
+//	const int& nStudent, const int& nGroup)
+//{
+//	return 3 * pow(value, 2) / (2 * nStudent * nGroup);
+//}
+
+double BasicFunction (const double& value,
+	const int& nStudent, const int& nGroup)
+{
+	return (pow(value, 3) / (nStudent * nGroup)) +
+		2 * value - (nStudent + nGroup);
+}
+
+double BasicFunctionDevariative(const double& value,
+	const int& nStudent, const int& nGroup)
+{
+	return 3 * pow(value, 2) / (nStudent * nGroup) + 2;
+}
+
+int CheckSign(const double& firstBorder, const double& secondBorder,
+	const int& nStudent, const int& nGroup)
+{
+	return (BasicFunction(secondBorder, StudentParameters) -
+		BasicFunction(firstBorder, StudentParameters) > 0) ? -1 : 1;
+}
+
+double FindMaxValue(const double& firstBorder, const double& secondBorder,
+	const int& nStudent, const int& nGroup)
+{
+	return (abs(BasicFunctionDevariative(firstBorder, StudentParameters) >
+		abs(BasicFunctionDevariative(secondBorder, StudentParameters)))) ?
+			abs(BasicFunctionDevariative(firstBorder, StudentParameters)) :
+				abs(BasicFunctionDevariative(secondBorder, StudentParameters));
+}
+
+double EquivalentFuction(const double& value, const int& nStudent, 
+	const int& nGroup, const int& equationSign, const double& maxValue)
+{
+	return value + equationSign * 
+		BasicFunction(value, StudentParameters) / maxValue;
+}
+
+double EquivalentDevariative(const double& value, const int& nStudent,
+	const int& nGroup, const int& equationSign, const double& maxValue)
+{
+	return 1 + equationSign * BasicFunctionDevariative(value, StudentParameters) /
+		maxValue;
+}
+
+//double EquivalentDevariative(const double& value, const int& _firstBorder, const int& _secondBorder,
+//	const int& nStudent, const int& nGroup, const int& equationSign)
+//{
+//	return 1 + equationSign * BasicFunctionDevariative(value, StudentParameters) /
+//		FindMaxValue(Borders, StudentParameters);
+//}
+
+//double FindMaxValueForEquivalent()
+//{
+//	
+//}
+
+void IterationMethod(const double& _firstBorder, const double& _secondBorder, 
+	const int& nStudent, const int& nGroup)
+{
+	double maxBasicValue = FindMaxValue(Borders, StudentParameters); // FOR BETTER CALC
+	int equationSign = CheckSign(Borders, StudentParameters);	  // FOR BETTER CALC
+
+	cout << "\n";
+
+	double maxEqualValue;
+
+	if (abs(EquivalentDevariative(_firstBorder, StudentParameters, SignMax))
+		> abs(EquivalentDevariative(_secondBorder, StudentParameters, SignMax)))
+	{
+		maxEqualValue = abs(EquivalentDevariative(_firstBorder, StudentParameters, SignMax));
+	}
+	else
+	{
+		maxEqualValue = abs(EquivalentDevariative(_secondBorder, StudentParameters, SignMax));
+	}
+
+	/*try
+	{
+		if (!(maxEqualValue < 1) || (maxEqualValue == 0))
+		{
+			throw out_of_range("Q имеет неверное значение, Q = " + to_string(maxEqualValue));
+		}
+	} catch (exception& ex)
+	{
+		cout << ex.what() << endl;
+		exit(9);
+	}*/
+
+	static double validEps;
+
+	if (maxEqualValue == 0)
+	{
+		validEps = epsAcc;
+	}
+	else
+	{
+		validEps = (1 - maxEqualValue) * epsAcc / maxEqualValue;		// IN CASE, WHEN EPS IS KNOWN
+		cout << "Q = " << maxEqualValue << endl;
+	}
+
+	double _xO = (_firstBorder - _secondBorder) / 2,
+		_xN = EquivalentFuction(_xO, StudentParameters, SignMax),
+		absDiff = fabs(_xN - _xO);
+
+	while (absDiff >= validEps) 
+	{
+		_xO = _xN;
+		_xN = EquivalentFuction(_xO, StudentParameters, SignMax);
+		absDiff = fabs(_xN - _xO); // ** //
+		++simpleIterCount;
+	}
+
+	cout << "\n";
+	double _root = _xN;
+
+	cout << "Корень " << setprecision(7) << fixed << _root << endl << "Количество итераций: "
+		<< simpleIterCount << endl;
+
+	cout << "\n";
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	double _firstBorder, _secondBorder;
+	cout << "Нахождение корня методом простых итераций" << endl
+		<< "Введите интервал a и b (через пробел): " << endl;
+	cin >> _firstBorder >> _secondBorder;
+
+	cout << "Введите номер группы и номер студента через пробел: " << endl;
+	int nGroup, nStudent;
+	cin >> nGroup >> nStudent;
+
+	IterationMethod(Borders, StudentParameters);
+	
+	return 0;
+	
+}
---
+++ 6. Interpolation/Interpolation.cpp
@@ -0,0 +1,276 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+#include <iomanip>
+#include <random>
+
+using namespace std;
+
+#define StudentData nGroup, nStudent
+
+uniform_int_distribution<> uid(-10, 10);
+
+// x = -1 , 1, 5
+// y =  4, -2, 10
+
+double** ThrowInPower(double** _matrix, const int& node)
+{
+	for (int i = 0; i < node; ++i)
+	{
+		for (int j = 0; j < node; ++j)
+		{
+			int _power = node - j - 1;
+			_matrix[i][j] = pow(_matrix[i][j], _power);
+		}
+	}
+
+	return _matrix;
+}
+
+double** CreateMatrix(const int& node, const int& nGroup, const int& nStudent, const int& inputType)
+{
+	setlocale(LC_ALL, "rus");
+
+	random_device dev;
+	mt19937 _randengine(dev());
+	
+	auto** _matrix = new double* [node];
+
+	for (int i = 0; i < node; ++i)
+	{
+		_matrix[i] = new double[node];
+	}
+
+		if (inputType == 0) // studentParameters
+		{
+			if (node > 5 || node < 1)
+			{
+				cout << "Неверное количество точек" << endl;
+				exit(10);
+			}
+			else
+			{
+				if (node >= 2 && node <= 5) 
+				{
+					_matrix[0][0] = -2;
+					_matrix[0][node] = nStudent;
+				}
+				if (node >= 2 && node <= 5) 
+				{
+					_matrix[1][0] = -1;
+					_matrix[1][node] = nGroup;
+				}
+				if (node >= 3 && node <= 5) 
+				{
+					_matrix[2][0] = 0;
+					_matrix[2][node] = -1;
+				}
+				if (node >= 4 && node <= 5) 
+				{
+					_matrix[3][0] = 1;
+					_matrix[3][node] = nStudent;
+				}
+				if (node == 5) 
+				{
+					_matrix[4][0] = 2;
+					_matrix[4][node] = nGroup;
+				}
+				for (int i = 0; i < node; ++i)
+				{
+					for (int j = 0; j < node; ++j)
+					{
+						_matrix[i][j] = _matrix[i][0];
+					}
+				}
+				cout << "X: ";
+				for (int i = 0; i < node; ++i)
+				{
+					cout << setw(7) << setprecision(2) << fixed << _matrix[i][0] << ' ';
+				}
+				cout << endl << "Y: ";
+				for (int i = 0; i < node; ++i)
+				{
+					cout << setw(7) << setprecision(2) << fixed << _matrix[i][node] << ' ';
+				}
+				cout << endl << endl;
+			}
+		}
+		else if (inputType == 1) // Random
+		{
+			for (int i = 0; i < node; ++i)
+			{
+				for (int j = 0; j < node; ++j)
+				{
+					_matrix[i][j] = uid(_randengine);
+				}
+				_matrix[i][node] = uid(_randengine);
+			}
+			cout << "X: ";
+			for (int i = 0; i < node; ++i)
+			{
+				cout << setw(7) << setprecision(2) << fixed << _matrix[i][0] << ' ';
+			}
+			cout << endl << "Y: ";
+			for (int i = 0; i < node; ++i)
+			{
+				cout << setw(7) << setprecision(2) << fixed << _matrix[i][node] << ' ';
+			}
+			cout << endl << endl;
+		}
+
+		return ThrowInPower(_matrix, node);
+
+}
+
+//double* DecisionMatrix(const int& node, const int& nStudent, const int& nGroup)
+//{
+//	setlocale(LC_ALL, "rus");
+//
+//	random_device dev;
+//	mt19937 _randengine(dev());
+//	
+//	auto* _decMatrix = new double[node];
+//
+//	_decMatrix[0] = 4;
+//	_decMatrix[1] = -2;
+//	_decMatrix[2] = 10;
+//
+//	return _decMatrix;
+//}
+
+void PrintMatrix (double** _matrix, const int& node)
+{
+	for (size_t i = 0; i < node; ++i)
+	{
+		for (size_t j = 0 ; j < node; ++j)
+		{
+			cout << setw(4 + node - j) << _matrix[i][j] << " * x ^ " + to_string(node-j) << ' ';
+		}
+		cout << setw(node + 1) << _matrix[i][node] << ' ' << endl;
+	}
+	cout << "\n";
+}
+
+double* FindRoots(double** _matrix, const int& node)
+{
+	setlocale(LC_ALL, "rus");
+
+	double* roots = new double[node], summ = 0;
+	for (int iter = 0; iter < node; iter++)
+	{
+		for (int i = iter; i < node; i++)
+		{
+			double temp = _matrix[i][iter];
+
+			if (temp != 0)
+			{
+				for (int j = iter; j < node; j++) _matrix[i][j] = _matrix[i][j] / temp;
+				_matrix[i][node] = _matrix[i][node] / temp;
+			}
+		}
+		for (int i = iter + 1; i < node; i++)
+		{
+			if (_matrix[i][iter] != 0)
+			{
+				for (int j = iter; j < node; j++)
+				{
+					_matrix[i][j] = _matrix[i][j] - _matrix[iter][j];
+					summ = summ + abs(_matrix[i][j]);
+				}
+
+				_matrix[i][node] = _matrix[i][node] - _matrix[iter][node];
+
+				/*if (summ == 0)
+				{
+					cout << "exception";
+					exit(9);
+				}*/
+			}
+		}
+	}
+
+	summ = 0;
+
+	for (int iter = node - 1; iter >= 0; iter--) {
+		roots[iter] = _matrix[iter][node] - summ;
+		if (iter > 0) {
+			for (int i = node - 1; i >= iter; i--) summ = _matrix[iter - 1][i] * roots[i];
+		}
+	}
+	return roots;
+}
+
+//void InterpolationMethod(double** _matrix, double* _roots, const int& node)
+//{
+//	for (int i = 0; i < node; ++i)
+//	{
+//		for (int j = 0; j < node; ++j)
+//		{
+//			_matrix[i][j] = _roots[i];
+//		}
+//	}
+//
+//	PrintMatrix(_matrix, _roots, node);
+//}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	cout << "Составление интерполяционного многочлена по n - узлам." << endl;
+	cout << "Введите количество узлов, номер группы и номер студента, тип ввода(через пробел)" << endl;
+
+	int node, nGroup, nStudent, inputType; // nStudent & nGroup defined as StudentData;
+	cin >> node >> nGroup >> nStudent >> inputType;
+	cout << "\n";
+
+	if (inputType != 0 && inputType != 1)
+	{
+		cout << "Неверный тип ввода" << endl;
+		exit(10);
+	}
+
+	auto** _matrix = CreateMatrix(node, StudentData, inputType);
+
+	PrintMatrix(_matrix, node);
+
+	auto* roots = FindRoots(_matrix, node);
+
+	cout << "Корни системы: ";
+	for (int i = 0; i < node; ++i)
+	{
+		cout << setw(5) << roots[i] << ' ';
+	}
+
+	cout << endl << "Полученный многочлен: P(x) = ";
+
+	for (int i = 0; i < node; ++i)																									// TO REWORK COUT
+	{																																
+		if (node - i != 1)																											
+		{																															
+			if(roots[i] > 1)																										
+			{																														
+				cout << setw(node - i) << setprecision(3) << fixed << roots[i] << " * x ^ " + to_string(node - i) << ' ';			
+			}																														
+			else if(roots[i] < 1)																									
+			{																														
+				cout << setw(node - i) << setprecision(3) << fixed << roots[i] << " * x ^ " + to_string(node - i) << ' ';			
+			}																														
+		}																															
+		else																														
+		{																															
+			if(roots[i] > 1)																										
+			{																														
+				cout << setw(node - i) << setprecision(3) << fixed << roots[i] << " * x";											
+			}																														
+			else if (roots[i] < 1)																									
+			{																														
+				cout << setw(node - i) << setprecision(3) << fixed << roots[i] << " * x";											
+			}																														
+		}																															
+	}																																// TO REWORK COUT
+																																	
+	cout << endl;
+	
+	return 0;
+}
